d10609dd3275663be8340972e0570cba9a2fa0f7
h2jinee <wjsgmlwls97@gmail.com>
2023-12-28 17:20:00 +0900

    실전 예제 1 - 요구사항 분석과 기본 매핑

--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -1,2 +1,17 @@
-package jpabook.jpashop.domain;public class Item {
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Item {
+    @Id @GeneratedValue
+    @Column(name = "ITEM_ID")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
 }
